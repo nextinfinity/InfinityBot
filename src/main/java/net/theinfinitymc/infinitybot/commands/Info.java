ae2f8ae0aaa4a08795c8420614d26b10a3e7a527
Next Infinity <xgix57@gmail.com>
2020-05-28 23:04:37 -0400

    Bump dependencies

--- src/main/java/net/theinfinitymc/infinitybot/commands/Info.java
+++ src/main/java/net/theinfinitymc/infinitybot/commands/Info.java
@@ -10,7 +10,7 @@ public class Info implements Command {
 	public void execute(MessageReceivedEvent event, String[] args) {
 		Message text = new MessageBuilder().append("__**Infinity Bot**__"
 				+ "\n" + "Version: " + "2.1.2"
-				+ "\n" + "API: JDA v" + "4.1.1_150" + " w/ LavaPlayer v" + "1.3.47"
+				+ "\n" + "API: JDA v" + "4.1.1_155" + " w/ LavaPlayer v" + "1.3.49"
 				+ "\n" + "Creator: NextInfinity").build();
 		event.getChannel().sendMessage(text).queue();
 	}
